3d5f44f5a2668c6e5f9e108971fe6412fb724940
carlosescom <carlos.escom.ipn@gmail.com>
2020-04-21 18:01:11 -0500

    update model

--- .history/lib/model_20200417164159.js
+++ .history/lib/model_20200417164159.js
@@ -1,7 +1,5 @@
 var Joi = require('joi');
 module.exports = {
-  sex      : Joi.string().email().required().max(10),
-  phone    : Joi.string().email().required().max(15),
   email    : Joi.string().email().required().max(254), // Required
   password : Joi.string().required().min(6).max(100),  // minimum length 6 characters]
   name     : Joi.string().optional().max(100),
